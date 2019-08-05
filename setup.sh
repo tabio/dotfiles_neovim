824c361c65d975bd6586f5d33e6d8d5f6f874486
tabio <tabio.github@gmail.com>
2019-08-05 22:49:50 +0900

    add coc.nvim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 brew install neovim
 brew install ctags
 
+gem install solargraph
+
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
 sh ./installer.sh ~/.cache/dein
